10d55ae029b3e11a6dc95ccf7b88d149c456fdbb
Fariz <Fariz@LAPTOP-JQ2AQ0I3>
2018-09-28 09:06:28 +0700

    pembuatan halaman masuk

---
+++ src/ttslontong/FXMLDocumentController.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ttslontong;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Fariz
+ */
+public class FXMLDocumentController implements Initializable {
+    
+    @FXML
+    private Label label;
+    @FXML
+    private Button buttonMasuk;
+    
+   
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void bmasuk(ActionEvent event) {
+        try {
+            // Hide this current window (if this is what you want)
+            ((Node)(event.getSource())).getScene().getWindow().hide();
+
+            // Load the new fxml
+            FXMLLoader fxmlLoader = new FXMLLoader();
+            fxmlLoader.setLocation(getClass().getResource("FXMLutama.fxml"));
+            Scene scene = new Scene(fxmlLoader.load(), 494, 545);
+
+            // Create new stage (window), then set the new Scene
+            Stage stage = new Stage();
+            stage.setScene(scene);
+            stage.show();
+
+        } catch (IOException e) {
+            System.out.println("Failed to create new Window." + e);
+        }
+    }
+    
+}
